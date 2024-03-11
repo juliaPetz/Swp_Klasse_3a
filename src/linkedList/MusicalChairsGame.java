3a5f6a30bf6ea3b58740822acfae55630f1146f7
juliaPetz <julia.petz@hak-reutte.ac.at>
2024-03-11 16:04:23 +0100

    lesson 2024-03-11

---
+++ src/linkedList/MusicalChairsGame.java
@@ -0,0 +1,15 @@
+package linkedList;
+
+public class MusicalChairsGame {
+
+	public static void main(String[] args) {
+		
+		MusicalChairs mc1 = new MusicalChairs("Leo", "Max", "Paul", "Anna");
+		MusicalChairs mc2 = new MusicalChairs("Madeleine", "Benedikt");
+		
+		System.out.println(mc1);
+		System.out.println(mc2);
+
+	}
+
+}
